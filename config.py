4a34c6c25fdc60e3a3381473075ddabce61c03ef
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-08-28 10:48:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.caldwcg5mkux.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "abcde123"
+customdb = "employee"
+custombucket = "leongjingen-employee"
+customregion = "us-east-1"
+
